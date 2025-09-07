b8bce7deef6820b88d855a0282c9c717b56501f4
kojowiafe-dev <kojowiafe502@gmail.com>
2025-09-07 05:10:21 +0000

    installed ui dependencies

---
+++ web/src/components/AnimatedThemeToggler.jsx
@@ -0,0 +1,9 @@
+import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggler";
+
+export default function AnimatedThemeTogglerDemo() {
+  return (
+    <div>
+      <AnimatedThemeToggler />
+    </div>
+  );
+}
